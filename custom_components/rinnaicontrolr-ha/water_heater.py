98b54d8701e045f5274f4510b6be0875587c8cb6
Brad Barbour <barbourbj@gmail.com>
2021-06-30 13:03:34 -0500

    fixed missing import

--- custom_components/rinnaicontrolr-ha/water_heater.py
+++ custom_components/rinnaicontrolr-ha/water_heater.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 
 import voluptuous as vol
 
-from homeassistant.components.water_heater import WaterHeaterEntity
+from homeassistant.components.water_heater import WaterHeaterEntity, SUPPORT_TARGET_TEMPERATURE
 from homeassistant.core import callback
 from homeassistant.helpers import entity_platform
 
