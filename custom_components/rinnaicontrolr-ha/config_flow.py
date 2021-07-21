ea4ed27512545c5bf7505c4c33ff26063aedf662
Brad Barbour <barbourbj@gmail.com>
2021-07-20 21:20:31 -0500

    add options to configure celcius or fahrenheit

--- custom_components/rinnaicontrolr-ha/config_flow.py
+++ custom_components/rinnaicontrolr-ha/config_flow.py
@@ -51,6 +51,22 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             step_id="user", data_schema=DATA_SCHEMA, errors=errors
         )
 
+class OptionsFlow(config_entries.OptionsFlow):
+    def __init__(self, config_entry: config_entries.ConfigEntry):
+        """Initialize options flow."""
+        self.config_entry = config_entry
+
+    async def async_step_init(self, user_input=None):
+        if user_input is not None:
+            return self.async_create_entry(title="", data=user_input)
+        options = {
+            vol.Optional(
+                CONF_UNIT,
+                default=self.config_entry.options.get(CONF_UNIT, DEFAULT_UNIT),
+            ): vol.In(CONF_UNITS)
+        }
+
+        return self.async_show_form(step_id="init", data_schema=vol.Schema(options))
 
 class CannotConnect(exceptions.HomeAssistantError):
     """Error to indicate we cannot connect."""
