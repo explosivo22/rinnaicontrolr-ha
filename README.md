37c7d477ec7037c3972891107a9107c66082b393
Explosivo22 <barbourbj@gmail.com>
2025-01-08 10:43:36 -0600

    Rename folder to match domain. Cleanup Readme

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Support for [Rinnai Control-R Water Heater monitoring and control device](https:
 ![release_date](https://img.shields.io/github/release-date/explosivo22/rinnaicontrolr-ha?style=for-the-badge)
 [![License](https://img.shields.io/github/license/explosivo22/rinnaicontrolr-ha?style=for-the-badge)](https://opensource.org/licenses/Apache-2.0)
 [![hacs_badge](https://img.shields.io/badge/HACS-Custom-orange.svg?style=for-the-badge)](https://github.com/custom-components/hacs)
-
+[![HA integration usage](https://img.shields.io/badge/dynamic/json?color=41BDF5&logo=home-assistant&label=integration%20usage&suffix=%20installs&cacheSeconds=15600&url=https://analytics.home-assistant.io/custom_integrations.json&query=$.rinnai.total)](https://analytics.home-assistant.io/custom_integrations.json)
 <a href="https://www.buymeacoffee.com/Explosivo22" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-blue.png" alt="Buy Me A Coffee" style="height: 60px !important;width: 217px !important;" ></a>
 
 ## KNOWN ISSUES
@@ -56,14 +56,23 @@ Service | Parameters | Description
 The 'master' branch of this custom component is considered unstable, alpha quality and not guaranteed to work.
 Please make sure to use one of the official release branches when installing using HACS, see [what has changed in each version](https://github.com/explosivo22/rinnaicontrolr-ha/releases).
 
-### Install Custom Components
+#### With HACS
 
-1) Make sure that [Home Assistant Community Store (HACS)](https://github.com/custom-components/hacs) is setup.
-2) Go to integrations in HACS
-3) click the 3 dots in the top right corner and choose `custom repositories`
-4) paste the following into the repository input field `https://github.com/explosivo22/rinnaicontrolr-ha` and choose category of `Integration`
-5) click add and restart HA to let the integration load
-6) Recommended to clear the cache and reload first before adding the integration.
-7) Go to settings and choose `Devices & Services`.
-8) Click `Add Integration` and search for `Rinnai Control-R Water Heater`
-9) Configure the integration.
+[![Open your Home Assistant instance and open a repository inside the Home Assistant Community Store.](https://my.home-assistant.io/badges/hacs_repository.svg)](https://my.home-assistant.io/redirect/hacs_repository/?owner=explosivo22&repository=rinnaicontrolr-ha&category=integration)
+
+#### Manual
+1. Copy the `rinnai` directory from `custom_components` in this repository and place inside your Home Assistant's `custom_components` directory.
+2. Restart Home Assistant
+3. Follow the instructions in the `Setup` section
+
+> [!WARNING]
+> If installing manually, in order to be alerted about new releases, you will need to subscribe to releases from this repository.
+
+# Setup
+[![Open your Home Assistant instance and start setting up a new integration.](https://my.home-assistant.io/badges/config_flow_start.svg)](https://my.home-assistant.io/redirect/config_flow_start/?domain=rinnai)
+
+> [!Tip]
+> If you are unable to use the button above, follow the steps below:
+> 1. Navigate to the Home Assistant Integrations page (Settings --> Devices & Services)
+> 2. Click the `+ ADD INTEGRATION` button in the lower right-hand corner
+> 3. Search for `Rinnai Control-R Water Heater`
---
+++ custom_components/rinnai/sensor.py
@@ -0,0 +1,260 @@
+"""Support for Rinnai Water Heater Monitor sensors."""
+from __future__ import annotations
+
+from homeassistant.const import (
+    UnitOfTemperature,
+    UnitOfElectricCurrent,
+    UnitOfFrequency,
+)
+
+from homeassistant.components.sensor import (
+    SensorStateClass,
+    SensorDeviceClass,
+    SensorEntity
+)
+
+from .const import DOMAIN as RINNAI_DOMAIN, CONF_UNIT
+from .device import RinnaiDeviceDataUpdateCoordinator
+from .entity import RinnaiEntity
+
+GAUGE_ICON = "mdi:gauge"
+COMBUSTION_ICON = "mdi:fire-circle"
+OPERATION_ICON = "mdi:home-lightning-bolt-outline"
+PUMP_ICON = "mdi:pump"
+PUMP_CYCLES_ICON = "mdi:heat-pump-outline"
+FAN_CURRENT_ICON = "mdi:fan-auto"
+FAN_FREQUENCY_ICON = "mdi:fan-chevron-up"
+
+async def async_setup_entry(hass, config_entry, async_add_entities):
+    """Set up the Rinnai sensors from config entry."""
+    devices: list[RinnaiDeviceDataUpdateCoordinator] = hass.data[RINNAI_DOMAIN][
+        config_entry.entry_id
+    ]["devices"]
+    entities = []
+    for device in devices:
+        entities.extend(
+            [
+                RinnaiOutletTemperatureSensor(device),
+                RinnaiInletTemperatureSensor(device),
+                RinnaiWaterFlowRateSensor(device),
+                RinnaiCombustionCyclesSensor(device),
+                RinnaiOperationHoursSensor(device),
+                RinnaiPumpHoursSensor(device),
+                RinnaiPumpCyclesSensor(device),
+                RinnaiFanCurrentSensor(device),
+                RinnaiFanFrequencySensor(device),
+            ]
+        )
+    async_add_entities(entities)
+
+class RinnaiOutletTemperatureSensor(RinnaiEntity, SensorEntity):
+    """Monitors the temperature."""
+
+    def __init__(self, device):
+        """Initialize the temperature sensor."""
+        super().__init__("outlet_temperature", f"{device.device_name} Outlet Temperature", device)
+        self._state: float = None
+
+    @property
+    def device_class(self):
+        """Return the device class of the sensor."""
+        return SensorDeviceClass.TEMPERATURE
+
+    @property
+    def state_class(self):
+        """Return the state class of the sensor."""
+        return SensorStateClass.MEASUREMENT
+
+    @property
+    def native_unit_of_measurement(self) -> str | None:
+        return UnitOfTemperature.FAHRENHEIT
+
+    @property
+    def native_value(self):
+        """Return the current temperature."""
+        if self._device.outlet_temperature is None:
+            return None
+        return round(self._device.outlet_temperature, 1)
+
+class RinnaiInletTemperatureSensor(RinnaiEntity, SensorEntity):
+    """Monitors the temperature."""
+
+    def __init__(self, device):
+        """Initialize the temperature sensor."""
+        super().__init__("inlet_temperature", f"{device.device_name} Inlet Temperature", device)
+        self._state: float = None
+
+    @property
+    def device_class(self):
+        """Return the device class of the sensor."""
+        return SensorDeviceClass.TEMPERATURE
+
+    @property
+    def state_class(self):
+        """Return the state class of the sensor."""
+        return SensorStateClass.MEASUREMENT
+
+    @property
+    def native_unit_of_measurement(self) -> str | None:
+        return UnitOfTemperature.FAHRENHEIT
+
+    @property
+    def native_value(self):
+        """Return the current temperature."""
+        if self._device.inlet_temperature is None:
+            return None
+        return round(self._device.inlet_temperature, 1)
+
+class RinnaiWaterFlowRateSensor(RinnaiEntity, SensorEntity):
+    """Monitors the water flow rate."""
+
+    _attr_icon = GAUGE_ICON
+    _attr_native_unit_of_measurement = "gpm"
+    _attr_state_class: SensorStateClass = SensorStateClass.MEASUREMENT
+
+    def __init__(self, device):
+        """Initialize the temperature sensor."""
+        super().__init__("water_flow_rate", f"{device.device_name} Water Flow Rate", device)
+        self._state: float = None
+
+    @property
+    def native_value(self):
+        """Return the current temperature."""
+        if self._device.water_flow_rate is None:
+            return None
+        return round(self._device.water_flow_rate * 0.1, 1)
+
+class RinnaiCombustionCyclesSensor(RinnaiEntity, SensorEntity):
+    """Monitors the combustion cycles."""
+
+    _attr_icon = COMBUSTION_ICON
+    _attr_native_unit_of_measurement = "cycles"
+    _attr_state_class: SensorStateClass = SensorStateClass.MEASUREMENT
+
+    def __init__(self, device):
+        """Initialize the temperature sensor."""
+        super().__init__("combustion_cycles", f"{device.device_name} Combustion Cycles x100", device)
+        self._state: float = None
+
+    @property
+    def native_value(self):
+        """Return the current temperature."""
+        if self._device.combustion_cycles is None:
+            return None
+        return round(self._device.combustion_cycles, 1)
+
+class RinnaiOperationHoursSensor(RinnaiEntity, SensorEntity):
+    """Monitors the operation hours."""
+
+    _attr_icon = OPERATION_ICON
+
+    def __init__(self, device):
+        """Initialize the temperature sensor."""
+        super().__init__("operation_hours", f"{device.device_name} Operation Hours x100", device)
+        self._state: float = None
+
+    @property
+    def state_class(self):
+        """Return the state class of the sensor."""
+        return SensorStateClass.MEASUREMENT
+
+    @property
+    def native_value(self):
+        """Return the current temperature."""
+        if self._device.operation_hours is None:
+            return None
+        return round(self._device.operation_hours, 1)
+
+class RinnaiPumpHoursSensor(RinnaiEntity, SensorEntity):
+    """Monitors the pump hours."""
+
+    _attr_icon = PUMP_ICON
+
+    def __init__(self, device):
+        """Initialize the temperature sensor."""
+        super().__init__("pump_hours", f"{device.device_name} Pump Hours x100", device)
+        self._state: float = None
+
+    @property
+    def state_class(self):
+        """Return the state class of the sensor."""
+        return SensorStateClass.MEASUREMENT
+
+    @property
+    def native_value(self):
+        """Return the current temperature."""
+        if self._device.pump_hours is None:
+            return None
+        return round(self._device.pump_hours, 1)
+
+class RinnaiPumpCyclesSensor(RinnaiEntity, SensorEntity):
+    """Monitors the pump cycles."""
+
+    _attr_icon = PUMP_CYCLES_ICON
+    _attr_native_unit_of_measurement = "cycles"
+    _attr_state_class: SensorStateClass = SensorStateClass.MEASUREMENT
+
+    def __init__(self, device):
+        """Initialize the temperature sensor."""
+        super().__init__("pump_cycles", f"{device.device_name} Pump Cycles x100", device)
+        self._state: float = None
+
+    @property
+    def native_value(self):
+        """Return the current temperature."""
+        if self._device.pump_cycles is None:
+            return None
+        return round(self._device.pump_cycles, 1)
+
+class RinnaiFanCurrentSensor(RinnaiEntity, SensorEntity):
+    """Monitors the fan current."""
+
+    _attr_icon = FAN_CURRENT_ICON
+
+    def __init__(self, device):
+        """Initialize the temperature sensor."""
+        super().__init__("fan_current", f"{device.device_name} Fan Current x10", device)
+        self._state: float = None
+
+    @property
+    def state_class(self):
+        """Return the state class of the sensor."""
+        return SensorStateClass.MEASUREMENT
+
+    @property
+    def native_unit_of_measurement(self) -> str | None:
+        return UnitOfElectricCurrent.MILLIAMPERE
+
+
+    @property
+    def native_value(self):
+        """Return the current temperature."""
+        if self._device.fan_current is None:
+            return None
+        return round(self._device.fan_current, 1)
+
+class RinnaiFanFrequencySensor(RinnaiEntity, SensorEntity):
+    """Monitors the fan frequency"""
+
+    _attr_icon = FAN_FREQUENCY_ICON
+
+    def __init__(self, device):
+        """Initialize the temperature sensor."""
+        super().__init__("fan_frequency", f"{device.device_name} Fan Frequency", device)
+        self._state: float = None
+
+    @property
+    def state_class(self):
+        """Return the state class of the sensor."""
+        return SensorStateClass.MEASUREMENT
+
+    @property
+    def native_unit_of_measurement(self) -> str | None:
+        return UnitOfFrequency.HERTZ
+
+    @property
+    def native_value(self):
+        """Return the current temperature."""
+        if self._device.fan_frequency is None:
+            return None
+        return round(self._device.fan_frequency, 1)
